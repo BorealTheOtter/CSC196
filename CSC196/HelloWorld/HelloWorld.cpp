ee22430368792eceefb828a67e7889cb299832d6
thethomasuser93 <thethomasuser93@gmail.com>
2026-06-30 09:26:08 -0600

    Setup a game engine solution

--- CSC196/HelloWorld/HelloWorld.cpp
+++ CSC196/HelloWorld/HelloWorld.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << numeric_limits<int>::max() << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
